452730c00fefddc7b23e6f534ff5dea798ea31fa
Illia-Illia <Takoexerrorx@gmail.com>
2022-07-23 21:09:37 +0300

    finish task 9

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,8 @@
+const amountEl = document.querySelector('#controls');
+amountEl.addEventListener('input', event => {
+  console.log(event.currentTarget.value);
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
