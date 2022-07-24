efad142e98bd380fa8b3d3a060688017e90da5ba
Illia-Illia <Takoexerrorx@gmail.com>
2022-07-24 13:14:32 +0300

    finish 10 task

--- js/task-10.js
+++ js/task-10.js
@@ -1,8 +1,38 @@
-const amountEl = document.querySelector('#controls');
-amountEl.addEventListener('input', event => {
-  console.log(event.currentTarget.value);
+const refs = {
+  amountEl: document.querySelector('#controls input'),
+  buttonCreateEl: document.querySelector('button[data-create]'),
+  buttonDesrtoyEl: document.querySelector('button[data-destroy]'),
+  addEl: document.querySelector('#boxes'),
+};
+
+refs.amountEl.addEventListener('change', event => {
+  let amount = event.currentTarget.value;
+  refs.buttonCreateEl.addEventListener('click', () => {
+    createBoxes(amount);
+    refs.amountEl.value = '';
+    amount = '';
+  });
+});
+
+refs.buttonDesrtoyEl.addEventListener('click', () => {
+  refs.addEl.innerHTML = '';
 });
 
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function createBoxes(amount) {
+  console.log(amount);
+  let size = 20;
+  for (let i = 0; i < amount; i++) {
+    size += 10;
+    const divboxEl = document.createElement('div');
+    divboxEl.style.width = `${size}px`;
+    divboxEl.style.height = `${size}px`;
+    divboxEl.style.backgroundColor = getRandomHexColor();
+    refs.addEl.append(divboxEl);
+  }
 }
